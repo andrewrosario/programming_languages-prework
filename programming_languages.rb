8e9ee60f1709e94a1636a30e893b74e8ba9cb419
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-26 22:20:36 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,38 +1,4 @@
-languages = {
-      :oo => {
-        :ruby => {
-          :type => "interpreted"
-        },
-        :javascript => {
-          :type => "interpreted"
-        },
-        :python => {
-          :type => "interpreted"
-        },
-        :java => {
-          :type => "compiled"
-        }
-      },
-      :functional => {
-        :clojure => {
-          :type => "compiled"
-        },
-        :erlang => {
-          :type => "compiled"
-        },
-        :scala => {
-          :type => "compiled"
-        },
-        :javascript => {
-          :type => "interpreted"
-        }
-     
-      }
-    }
-    
-    
-    
-    
+   
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, hash|
@@ -54,33 +20,3 @@ def reformat_languages(languages)
   end
   new_hash
 end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-# def reformat_languages(languages)
-#   # your code here
-#   i = 0
-#   new_hash = {}
-#   languages.map do |obj, style|
-#     languages[obj].map do |language, type|
-#       new_hash[language] = type
-#       if i > 0
-#         new_hash[language][:style] << obj
-#       else
-#         new_hash[language][:style] = [obj]
-#       end
-#       puts new_hash
-#     end
-#   end
-#   new_hash
-# end
