7e116d8899ea06709ca84a8592f4625256cb34bb
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-26 22:12:45 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -34,14 +34,17 @@ languages = {
     
     
 def reformat_languages(languages)
+  new_hash
   languages.each do |style, hash|
     hash.each do |language_names, type_hash|
+      if !new_hash.key?(language_names)
+        new_hash[language_names] = {}
+      end
       type_hash.each do |type_symbol, name_of_type|
-        puts type_symbol
-        puts name_of_type
       end
     end
   end
+  new_hash
 end
     
     
