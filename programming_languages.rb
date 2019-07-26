96c76e1702bf4ae07dfef1a8fcd17a40a549b9af
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-26 21:58:18 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,30 @@
+languages = {
+      :oo => {
+        :javascript => {
+          :type => "interpreted"
+        }
+      },
+      :functional => {
+        :javascript => {
+          :type => "interpreted"
+        }
+      }
+    }
+
 def reformat_languages(languages)
   # your code here
+  i = 0
+  new_hash = {}
+  languages.map do |obj, style|
+     languages[obj].map do |language, type|
+      new_hash[language] = type
+      if i > 0
+        new_hash[language][:style] << obj
+      else
+        new_hash[language][:style] = [obj]
+      end
+      puts new_hash
+     end
+  end
+  new_hash
 end
