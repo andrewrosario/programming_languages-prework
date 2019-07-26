fc1009687f6f888b654bc563fd7c54032e4c15d7
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-26 22:00:24 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -10,21 +10,43 @@ languages = {
         }
       }
     }
-
+    
+    
 def reformat_languages(languages)
-  # your code here
-  i = 0
-  new_hash = {}
-  languages.map do |obj, style|
-     languages[obj].map do |language, type|
-      new_hash[language] = type
-      if i > 0
-        new_hash[language][:style] << obj
-      else
-        new_hash[language][:style] = [obj]
-      end
-      puts new_hash
-     end
+  languages.each do |style, hash|
+    style.each do |
+    
   end
-  new_hash
+      
+      
 end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+# def reformat_languages(languages)
+#   # your code here
+#   i = 0
+#   new_hash = {}
+#   languages.map do |obj, style|
+#     languages[obj].map do |language, type|
+#       new_hash[language] = type
+#       if i > 0
+#         new_hash[language][:style] << obj
+#       else
+#         new_hash[language][:style] = [obj]
+#       end
+#       puts new_hash
+#     end
+#   end
+#   new_hash
+# end
